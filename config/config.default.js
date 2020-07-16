5521f2cb13def2693c5b33e0846ea98bd36ab091
junxie <jy.xj@alibaba-inc.com>
2020-07-16 14:23:26 +0800

    更新

--- config/config.default.js
+++ config/config.default.js
@@ -7,4 +7,20 @@ exports.view = {
         '.tpl': 'nunjucks',
     },
 };
+exports.alinode = {
+    enable: true,
+    package: 'egg-alinode'
+};
+
+exports.alinode = {
+    server: 'wss://agentserver.node.aliyun.com:8080',
+    appid: 85603,
+    secret: '1c53aa170f14d6e7409ec837ead8f2131032b9fe',
+    logdir: '/tmp/',
+    error_log: [
+        '您的应用在业务层面产生的异常日志的路径，数组，可选，可配置多个',
+        '例如：/root/.logs/error.#YYYY#-#MM#-#DD#.log',
+        '不更改 Egg 默认日志输出路径可不配置本项目',
+    ],
+};
 
