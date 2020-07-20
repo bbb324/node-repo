39fd483fbc6e9ed0ca67e477edfd5a11ceba6898
junxie <jy.xj@alibaba-inc.com>
2020-07-20 13:48:52 +0800

    更新脚本

--- app/controller/api/processControl.js
+++ app/controller/api/processControl.js
@@ -1,10 +1,10 @@
 const Controller = require('egg').Controller;
-const runScript = require('runscript');
+const execSync = require("child_process").execSync;
 
 class RepoController extends Controller {
     // 更新仓库脚本，用于github webhook
     async updateRepo() {
-        const {ctx} = this;
+        /* const {ctx} = this;
         const { repoLocation, repoName, branch = 'master' } = ctx.request.body;
         
         const res = await runScript(`git -C ${repoLocation}/${repoName} pull origin ${branch}`);
@@ -12,7 +12,32 @@ class RepoController extends Controller {
         ctx.body = {
             code: 0,
             data: res
+        } */
+
+
+        const {ctx} = this;
+        const { repoLocation, repoName, branch = 'master' } = ctx.request.body;
+        let message = '';
+        /* exec(`cd ${repoLocation}/${repoName} && git pull origin ${branch}`, (error, stdout, stderr) => {        
+            if (error) {
+                message = error.message;
+            }
+            if (stderr) {
+                message = stderr;
+            }
+        }); */
+        
+        try {
+            execSync(`cd ${repoLocation}/${repoName} && git pull origin ${branch}`);
+        } catch(e) {
+            message = e;
         }
+        
+        ctx.body = {
+            code: 0,
+            data: message
+        }
+
     }  
 }
 
