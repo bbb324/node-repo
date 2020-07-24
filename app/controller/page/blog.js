4bb417e4e73aa643955aefd86c3271e2d941a3bf
junxie <jy.xj@alibaba-inc.com>
2020-07-24 17:41:14 +0800

    增加获取最新版本号

--- app/controller/page/blog.js
+++ app/controller/page/blog.js
@@ -3,7 +3,14 @@ const Controller = require('egg').Controller;
 class HomeController extends Controller {
     async index() {
         const { ctx } = this;
-        await ctx.render('page/blog.html')
+        let version = '';
+        try {
+            const res = await await ctx.curl('https://api.github.com/repos/bbb324/bbb324.github.io/releases/latest',  { dataType: 'json' })
+            version = res.data.tag_name;
+        } catch(e) {
+            version = '0.0.3';
+        }
+        await ctx.render('page/blog.html', {version})
     }
 
 }
