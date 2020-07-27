758693885d04f8c1aa182b7580aa4ff877ccaa3d
junxie <jy.xj@alibaba-inc.com>
2020-07-27 18:08:59 +0800

    更新最小依赖版本

--- app/controller/page/blog.js
+++ app/controller/page/blog.js
@@ -3,12 +3,12 @@ const Controller = require('egg').Controller;
 class HomeController extends Controller {
     async index() {
         const { ctx } = this;
-        let version = '0.0.1';
+        let version = '0.0.7';
         try {
             const res = await await ctx.curl('https://api.github.com/repos/bbb324/bbb324.github.io/releases/latest',  { dataType: 'json' })
             version = res.data.tag_name;
         } catch(e) {
-            version = '0.0.1';
+            version = '0.0.7';
         }
         await ctx.render('page/blog.html', {version})
     }
