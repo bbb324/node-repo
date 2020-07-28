138e6ab0c0140fd1f6e8cca35400ce98e578876c
junxie <jy.xj@alibaba-inc.com>
2020-07-28 15:05:35 +0800

    增加错误链接转链

--- app/middleware/notFoundHandler.js
+++ app/middleware/notFoundHandler.js
@@ -1,6 +1,9 @@
 module.exports = () => {
     return async function notFoundHandler(ctx, next) {
       await next();
+      if(/\/$/.test(ctx.path)) {
+        ctx.redirect(ctx.path.slice(0, ctx.path.length - 1))
+      }
       if (ctx.status === 404 && !ctx.body) {
         if (ctx.acceptJSON) {
           ctx.body = { error: 'Not Found' };
@@ -8,7 +11,6 @@ module.exports = () => {
             if(ctx.path.indexOf('/blog.htm') >= 0) {
                 ctx.redirect('/blog.htm')
             }
-          
         }
       }
     };
